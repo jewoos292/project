ea0664764a201ba87f0edcf1d7dc8590e7f0081f
jewoo <jewoos2921@gmail.com>
2020-12-08 13:18:31 +0900

    django courser18

--- prog/acc/urls.py
+++ prog/acc/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import hello
 
+app_name = "account"
 urlpatterns = [
     path("hello/", hello, name='hello')
 ]
--- prog/acc/views.py
+++ prog/acc/views.py
@@ -1,7 +1,19 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from .models import HelloWorld
+from django.urls import reverse
 
 # Create your views here.
 def hello(request):
-    return render(request, "acc/hello.html")
+    if request.method == "POST":
+
+        temp = request.POST.get("hello_world_input")
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+        hello_world_list = HelloWorld.objects.all()
+
+        return HttpResponseRedirect(reverse("account:hello"))
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, "acc/hello.html", context={'hello_world_list': hello_world_list})
